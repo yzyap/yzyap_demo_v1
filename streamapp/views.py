fd78c625aa21c2b06c99e3c0579c34673e8dd443
mg <mumingozutok@gmail.com>
2020-08-18 18:52:56 +0300

    compiler projesi ile görsel birleştirmeler yapıldı.

--- streamapp/views.py
+++ streamapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
 from streamapp.camera import VideoCamera, IPWebCam
+from . import forms
 # Create your views here.
 
 def index(request):
-	return render(request, 'streamapp/home.html')
+	template_data = {}
+	form = forms.CodeExecutorForm()
+	template_data['form'] = form
+	return render(request, 'streamapp/home.html', template_data)
 
 def gen(camera):
 	while True:
