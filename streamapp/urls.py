cfa831e3f9d1c2e931866f6b1a671430253fa3a1
mg <mumingozutok@gmail.com>
2020-08-25 16:15:22 +0300

    Kullanıcının yazdığı kodu çalıştırması için Flask server hayata geçirildi.

--- streamapp/urls.py
+++ streamapp/urls.py
@@ -5,5 +5,4 @@ from streamapp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('video_feed', views.video_feed, name='video_feed'),
-    path('video_feed_processed', views.video_feed_processed, name='video_feed_processed'),
     ]
--- streamapp/views.py
+++ streamapp/views.py
@@ -1,67 +1,54 @@
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
 from django.http import HttpResponse
-from streamapp.camera import VideoCamera, IPWebCam
 from . import forms
-from . import CompilerUtils
-from .CompilerUtils import Compiler
+from . import code_server_comm
+from .code_server_comm import codeServerComm
 import cv2
 import numpy as np
-
-executor = Compiler()
-video = cv2.VideoCapture("http://192.168.1.43:8080/?action=stream")
+import requests
 
 # Create your views here.
 def index(request):
 	template_data = {}
+	csc = codeServerComm()
 	if request.method == 'POST':
-		if 'submit_code' in request.POST:
-			form = forms.CodeExecutorForm(request.POST)
-			if form.is_valid():
+		form = forms.CodeExecutorForm(request.POST)
+		if form.is_valid():
+			if 'submit_code' in request.POST:			
 				code = form.cleaned_data['code']
-				executor.set_code(code)
-				execution_result = executor.execute()
-				executor.delete_code_file()
-				return HttpResponse("Worked!")
-			else:
-				return HttpResponse("Cannot sanitize form data")
+				print(code)
+				csc.set_code(code)
+				runserver_result = csc.run_server()
+				template_data['form'] = form
+				template_data['log'] = "Code Server Worked"
+				return render(request, 'streamapp/home.html', template_data)
 
-		elif 'terminate_code' in request.POST:
-			if not executor:
-				executor.terminate()
-				return HttpResponse("Terminated!")
-			else:
-				return HttpResponse("No valid run session!")
+			elif 'terminate_code' in request.POST:
+					response = requests.get('http://0.0.0.0:5000/shutdown')
+					template_data['form'] = form
+					template_data['log'] = "Code Server Terminated"
+					return render(request, 'streamapp/home.html', template_data)
+		else:
+			template_data['form'] = form
+			template_data['log'] = "Cannot sanitize form data"
+			return render(request, 'streamapp/home.html', template_data)
+
 	else:
 		form = forms.CodeExecutorForm()
 		template_data['form'] = form
+		template_data['log'] = "New Code Project"
 		return render(request, 'streamapp/home.html', template_data)
 
-
-def gen(camera):
-	while True:
-		frame = camera.get_frame()
-		yield (b'--frame\r\n'
-				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-def gen1(camera):	
-	while True:
+def gen():
+	video = cv2.VideoCapture("http://192.168.1.43:8080/?action=stream")
+	while True:		
 		success, image = video.read()
-		# We are using Motion JPEG, but OpenCV defaults to capture raw images,
-		# so we must encode it into JPEG in order to correctly display the
-		# video stream.
-
-		edges = cv2.Canny(image,100,200)
-		ret, jpeg = cv2.imencode('.jpg', edges)
-
-		frame = jpeg.tobytes()
+		ret, jpeg = cv2.imencode('.jpg', image)
+		frame=jpeg.tobytes()
 		yield (b'--frame\r\n'
-				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')				
-
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')		
+	
 def video_feed(request):
-	return StreamingHttpResponse(gen(VideoCamera()),
-					content_type='multipart/x-mixed-replace; boundary=frame')
-
-def video_feed_processed(request):
-	return StreamingHttpResponse(gen1(VideoCamera()),
-					content_type='multipart/x-mixed-replace; boundary=frame')
+	return StreamingHttpResponse(gen(),
+					content_type='multipart/x-mixed-replace; boundary=frame')						
